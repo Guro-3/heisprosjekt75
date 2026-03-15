260e9a177f89ee86540ff4a782888bd69414dc12
MathiasSagerup <mathias.sagerup@hotmail.no>
2026-03-15 13:13:51 +0100

    mer endring i role election

--- RoleLogic/RoleManager/role_election.go
+++ RoleLogic/RoleManager/role_election.go
@@ -7,22 +7,42 @@ import (
 	"log"
 )
 
+func contains(peers []string, id string) bool {
+	for _, p := range peers {
+		if p == id {
+			return true
+		}
+	}
+	return false
+}
+
 func RoleElection(peers peers.PeerUpdate, e *types.Elevator, ps *types.PeerState, doorStartTimerCh chan int) {
 	ps.PrevRole = ps.Role
 
 	if len(peers.Peers) == 1 {
-		//	ps.PrimaryID = e.MyID
 		e.Mode = types.SingleElevator
+		ps.BackupID = ""
+		ps.Role = types.RoleNode
+
 		ElevatorP.SingleElevatorOrderRedelegation(e, doorStartTimerCh)
-		log.Println("Elevator mode: ", e.Mode)
-	} else {
-		ps.PrimaryID = peers.Peers[0]
-		e.Mode = types.PrimaryBackup
-		log.Println("Elevator mode: ", e.Mode)
+		log.Println("Elevator mode:", e.Mode)
+		log.Println("my role is none (single mode)")
+		return
 	}
 
-	if len(peers.Peers) >= 2 {
-		ps.BackupID = peers.Peers[1]
+	e.Mode = types.PrimaryBackup
+	log.Println("Elevator mode:", e.Mode)
+
+	if ps.PrimaryID == "" || !contains(peers.Peers, ps.PrimaryID) {
+		ps.PrimaryID = peers.Peers[0]
+	}
+
+	ps.BackupID = ""
+	for _, id := range peers.Peers {
+		if id != ps.PrimaryID {
+			ps.BackupID = id
+			break
+		}
 	}
 
 	switch e.MyID {
