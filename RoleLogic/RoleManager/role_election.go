d67083ecfa5a0dc33c84318fe3b341c18eb57fed
MathiasSagerup <mathias.sagerup@hotmail.no>
2026-03-15 13:38:58 +0100

    tilbake endring

--- RoleLogic/RoleManager/role_election.go
+++ RoleLogic/RoleManager/role_election.go
@@ -1,68 +1,33 @@
 package RoleManager
 
 import (
+	"fmt"
 	"heisprosjekt75/ElevatorP"
+	"heisprosjekt75/Network-go/network/localip"
 	"heisprosjekt75/Network-go/network/peers"
 	"heisprosjekt75/types"
 	"log"
+	"time"
 )
-func contains(peers []string, id string) bool {
-	for _, p := range peers {
-		if p == id {
-			return true
-		}
-	}
-	return false
-}
-
-func firstOther(peers []string, avoid string) string {
-	for _, p := range peers {
-		if p != avoid {
-			return p
-		}
-	}
-	return ""
-}
 
 func RoleElection(peers peers.PeerUpdate, e *types.Elevator, ps *types.PeerState, doorStartTimerCh chan int) {
 	ps.PrevRole = ps.Role
 
 	if len(peers.Peers) == 1 {
+		ps.PrimaryID = e.MyID
 		e.Mode = types.SingleElevator
-		ps.PrimaryID = ""
-		ps.BackupID = ""
-		ps.Role = types.RoleNode
-
 		ElevatorP.SingleElevatorOrderRedelegation(e, doorStartTimerCh)
-		log.Println("Elevator mode:", e.Mode)
-		log.Println("my role is none (single mode)")
-		return
+		log.Println("Elevator mode: ", e.Mode)
+		ip, _ := localip.LocalIP()
+		e.MyID = fmt.Sprintf("%d-%s", time.Now().UnixNano(), ip)
+	} else {
+		ps.PrimaryID = peers.Peers[0]
+		e.Mode = types.PrimaryBackup
+		log.Println("Elevator mode: ", e.Mode)
 	}
 
-	e.Mode = types.PrimaryBackup
-	log.Println("Elevator mode:", e.Mode)
-
-	// Behold gammel primary hvis den fortsatt finnes
-	if ps.PrimaryID == "" || !contains(peers.Peers, ps.PrimaryID) {
-		// Hvis en node nettopp kom tilbake, ikke gjør den til primary med en gang
-		if peers.New != "" {
-			other := firstOther(peers.Peers, peers.New)
-			if other != "" {
-				ps.PrimaryID = other
-			} else {
-				ps.PrimaryID = peers.Peers[0]
-			}
-		} else {
-			ps.PrimaryID = peers.Peers[0]
-		}
-	}
-
-	ps.BackupID = ""
-	for _, id := range peers.Peers {
-		if id != ps.PrimaryID {
-			ps.BackupID = id
-			break
-		}
+	if len(peers.Peers) >= 2 {
+		ps.BackupID = peers.Peers[1]
 	}
 
 	switch e.MyID {
