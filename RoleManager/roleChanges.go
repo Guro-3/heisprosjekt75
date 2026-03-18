67bfbcee66daa515acf68a5ac3a7a6ccf4b54676
Anders Sandvik <anders-01@live.no>
2026-03-18 07:18:02 +0100

    kobla ut noe

--- RoleManager/roleChanges.go
+++ RoleManager/roleChanges.go
@@ -7,7 +7,7 @@ import (
 )
 
 func RolesChanges(port string, incomingTCP chan messagestypes.Message, e *types.Elevator) {
-	if e.Ps.Role != types.RolePrimary {
+	/*if e.Ps.Role != types.RolePrimary {
 		if e.Ps.PrimaryListener != nil {
 			e.Ps.PrimaryListener.Close()
 			e.Ps.PrimaryListener = nil
@@ -17,7 +17,7 @@ func RolesChanges(port string, incomingTCP chan messagestypes.Message, e *types.
 	if e.Ps.PrimaryConn != nil {
 		e.Ps.PrimaryConn.Close()
 		e.Ps.PrimaryConn = nil
-	}
+	}*/
 
 	if e.Ps.Role == types.RolePrimary {
 		tcp.TcpStartPrimary(port, incomingTCP, e)
